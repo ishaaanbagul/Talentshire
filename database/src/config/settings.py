e064228a2ed72e75a1d12439cca7d24499401f0e
Ampcus-Mukesh <mukesh.pawar@ampcustech.com>
2025-12-05 19:00:32 +0530

    push database services

---
+++ database/src/config/settings.py
@@ -0,0 +1,10 @@
+# MongoDB Configuration
+MONGO_URL = "mongodb://localhost:27017"
+MONGO_DB_NAME = "talentshire"
+
+# PostgreSQL Configuration
+POSTGRES_DB_NAME = "talentshire"
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = "admin@123"
+POSTGRES_HOST = "localhost"
+POSTGRES_PORT = "5432"
